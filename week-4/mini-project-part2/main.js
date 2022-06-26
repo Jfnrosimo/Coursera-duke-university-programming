7762d991228a9f069ccc2e3bc41fb86691e651a2
slimJeppy <j.rosimo098@gmail.com>
2022-06-26 23:49:25 +0800

    modified html, js and css

--- week-4/mini-project-part2/main.js
+++ week-4/mini-project-part2/main.js
@@ -6,6 +6,8 @@ var image4 = null;
 var image5 = null;
 var image6 = null;
 var image7 = null;
+var image8 = null;
+var image9 = null;
 var canvas;
 
 function uploadImage() {
@@ -17,6 +19,8 @@ function uploadImage() {
     image5 = new SimpleImage(imageFile);
     image6 = new SimpleImage(imageFile);
     image7 = new SimpleImage(imageFile);
+    image8 = new SimpleImage(imageFile);
+    image9 = new SimpleImage(imageFile);
     canvas = document.getElementById("can");
     image.drawTo(canvas);
 }
@@ -153,18 +157,19 @@ function applyVerticalRainbow() {
 }
 
 function applyBlur() {
-    var blurImage = new SimpleImage(image7.getWidth(), image7.getHeight());
-    for (var pixel of image7.values()){
+    imageIsLoaded(image6);
+    var blurImage = new SimpleImage(image6.getWidth(), image6.getHeight());
+    for (var pixel of image6.values()){
         if(Math.random() >= 0.5) {
-            var oldpixel = image7.getPixel(pixel.getX(), pixel.getY());
+            var oldpixel = image6.getPixel(pixel.getX(), pixel.getY());
             blurImage.setPixel(pixel.getX(), pixel.getY(), oldpixel);
         }else{
             var dist = Math.floor(Math.random()*30-15);
             var newx = pixel.getX() + dist;
             var newy = pixel.getY() + dist;
-            if (newx > 0 && newx <= image7.getWidth()-1) {
-                if (newy > 0 && newy <= image7.getHeight()-1){
-                    var nearpixel = image7.getPixel(newx, newy);
+            if (newx > 0 && newx <= image6.getWidth()-1) {
+                if (newy > 0 && newy <= image6.getHeight()-1){
+                    var nearpixel = image6.getPixel(newx, newy);
                     blurImage.setPixel(pixel.getX(),pixel.getY(), nearpixel);
                 }else{
                     blurImage.setPixel(pixel.getX(),pixel.getY(), pixel);
@@ -175,4 +180,148 @@ function applyBlur() {
         }
     }
     blurImage.drawTo(canvas);
+}
+
+function applyRedHue() {
+    imageIsLoaded(image7);
+    for (var pixel of image7.values()){
+      var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue());
+      if (avg < 128){
+        pixel.setRed(2*avg);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      }else{
+        pixel.setRed(255);
+        pixel.setGreen(2*avg - 255);
+        pixel.setBlue(2*avg - 255);
+      }  
+    }
+    image7.drawTo(canvas);
+}
+
+function applyDarkHue() {
+    imageIsLoaded(image8);
+    for (var pixel of image8.values()){
+      var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue());
+      if (avg > 128){
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      }else{
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(255);
+      }  
+    }
+    image8.drawTo(canvas);
+}
+
+
+function applyGridLines() {
+    imageIsLoaded(image9);
+    var width = image9.getWidth();
+    var height = image9.getHeight();
+    for(var pixel of image9.values()){
+        if(pixel.getX() <= width/10 && pixel.getX() >= (width/10)-0.5 ){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 2*width/10 && pixel.getX() >= (2*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 3*width/10 && pixel.getX() >= (3*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 4*width/10 && pixel.getX() >= (4*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 5*width/10 && pixel.getX() >= (5*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 6*width/10 && pixel.getX() >= (6*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 7*width/10 && pixel.getX() >= (7*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 8*width/10 && pixel.getX() >= (8*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= 9*width/10 && pixel.getX() >= (9*width/10)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getX() <= width && pixel.getX() >= (width)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= height/10 && pixel.getY() >= (height/10)-0.9 ){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 2*height/10 && pixel.getY() >= (2*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 3*height/10 && pixel.getY() >= (3*height/10)-.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 4*height/10 && pixel.getY() >= (4*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 5*height/10 && pixel.getY() >= (5*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 6*height/10 && pixel.getY() >= (6*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 7*height/10 && pixel.getY() >= (7*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 8*height/10 && pixel.getY() >= (8*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= 9*height/10 && pixel.getY() >= (9*height/10)-0.9){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+        if(pixel.getY() <= height && pixel.getY() >= (height)-0.5){
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }
+    }
+    image9.drawTo(canvas);
 }
